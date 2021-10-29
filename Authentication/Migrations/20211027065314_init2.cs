78b2932eda0b169a169cda9666dfb537bb440937
unknown <selvacelciya@gmail.com>
2021-10-29 18:38:06 +0530

    first commit

---
+++ Authentication/Migrations/20211027065314_init2.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Authentication.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PortfolioID",
+                table: "Users",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "index",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "index");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "index",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PortfolioID",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "PortfolioID");
+        }
+    }
+}
